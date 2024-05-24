4daaf69c79aa5930e5a8e5f750eb77168566b121
lazzieecoderr <chakravarthyrrc@gmail.com>
2024-05-24 12:05:29 +0530

    completed

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+//importing space
+
+
+//declaration/initailization
+const app =express();
+const PORT = 4000;
+
+//Middleware
+app.use(express.json())
+
+//routes
+app.get('/',(req,res)=>{
+   // res.status(200).json({message:"Hi all welcome to our first node app"})
+   res.status(200).send(`<span style="background-color:Aqua;color:black;font-size:100px">Welcome to our first app in nodejs</span>`)
+})
+
+
+//running port
+app.listen(PORT,()=>{
+    console.log(`App is listening on the port ${PORT}`);
+})
